9ea55a72c11d084cde20f769a184e1e0312a467d
Elias Cho <33488019+TurtleBCA@users.noreply.github.com>
2020-03-12 17:30:59 -0400

    Basic Layout Implemented

--- index.js
+++ index.js
@@ -6,23 +6,141 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Main = function (_React$Component) {
-    _inherits(Main, _React$Component);
+var Name = function (_React$Component) {
+    _inherits(Name, _React$Component);
 
-    function Main() {
+    function Name() {
+        _classCallCheck(this, Name);
+
+        return _possibleConstructorReturn(this, (Name.__proto__ || Object.getPrototypeOf(Name)).apply(this, arguments));
+    }
+
+    _createClass(Name, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "nameArea" },
+                React.createElement(
+                    "h1",
+                    null,
+                    "Elias Cho"
+                )
+            );
+        }
+    }]);
+
+    return Name;
+}(React.Component);
+
+var PageButton = function (_React$Component2) {
+    _inherits(PageButton, _React$Component2);
+
+    function PageButton() {
+        _classCallCheck(this, PageButton);
+
+        return _possibleConstructorReturn(this, (PageButton.__proto__ || Object.getPrototypeOf(PageButton)).apply(this, arguments));
+    }
+
+    _createClass(PageButton, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "button",
+                { className: "pageButton" },
+                this.props.text
+            );
+        }
+    }]);
+
+    return PageButton;
+}(React.Component);
+
+var NavBar = function (_React$Component3) {
+    _inherits(NavBar, _React$Component3);
+
+    function NavBar() {
+        _classCallCheck(this, NavBar);
+
+        return _possibleConstructorReturn(this, (NavBar.__proto__ || Object.getPrototypeOf(NavBar)).apply(this, arguments));
+    }
+
+    _createClass(NavBar, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "navBar" },
+                React.createElement(PageButton, { text: "About" }),
+                React.createElement(PageButton, { text: "Music" }),
+                React.createElement(PageButton, { text: "Projects" }),
+                React.createElement(PageButton, { text: "Blog" })
+            );
+        }
+    }]);
+
+    return NavBar;
+}(React.Component);
+
+var Header = function (_React$Component4) {
+    _inherits(Header, _React$Component4);
+
+    function Header() {
+        _classCallCheck(this, Header);
+
+        return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+    }
+
+    _createClass(Header, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "header" },
+                React.createElement(Name, null),
+                React.createElement(NavBar, null)
+            );
+        }
+    }]);
+
+    return Header;
+}(React.Component);
+
+var Body = function (_React$Component5) {
+    _inherits(Body, _React$Component5);
+
+    function Body() {
+        _classCallCheck(this, Body);
+
+        return _possibleConstructorReturn(this, (Body.__proto__ || Object.getPrototypeOf(Body)).apply(this, arguments));
+    }
+
+    _createClass(Body, [{
+        key: "render",
+        value: function render() {
+            return this.props.page;
+        }
+    }]);
+
+    return Body;
+}(React.Component);
+
+var Main = function (_React$Component6) {
+    _inherits(Main, _React$Component6);
+
+    function Main(props) {
         _classCallCheck(this, Main);
 
-        return _possibleConstructorReturn(this, (Main.__proto__ || Object.getPrototypeOf(Main)).apply(this, arguments));
+        var _this6 = _possibleConstructorReturn(this, (Main.__proto__ || Object.getPrototypeOf(Main)).call(this, props));
+
+        _this6.state = { page: "about" };
+        return _this6;
     }
 
     _createClass(Main, [{
-        key: 'render',
+        key: "render",
         value: function render() {
-            return React.createElement(
-                'h1',
-                null,
-                'test'
-            );
+            return React.createElement(Header, null);
         }
     }]);
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,67 @@
-class Main extends React.Component {
+class Name extends React.Component {
     render() {
         return (
-            <h1>test</h1>
+            <div className="nameArea">
+                <h1>Elias Cho</h1>
+            </div>
+        );
+    }
+}
+
+class PageButton extends React.Component {
+    render() {
+        return (
+            <button className="pageButton">
+                {this.props.text}
+            </button>
+        );
+    }
+}
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <div className="navBar">
+                <PageButton text="About"/>
+                <PageButton text="Music"/>
+                <PageButton text="Projects"/>
+                <PageButton text="Blog"/>
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <Name />
+                <NavBar />
+            </div>
+        );
+    }
+}
+
+class Body extends React.Component {
+    render() {
+        const pageHTML = {};
+
+        return this.props.page;
+    }
+}
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {page: "about"};
+    }
+
+    render() {
+        return (
+            <div className="Main">
+                <Header />
+                <Body page=this.state.page />
+            </div>
         );
     }
 }
