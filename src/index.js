3193111b5424227b8d39c8addbaa8f31cd0dbc9f
Elias Cho <33488019+TurtleBCA@users.noreply.github.com>
2020-03-12 16:09:50 -0400

    React Test

---
+++ src/index.js
@@ -0,0 +1,9 @@
+class Main extends React.Component {
+    render() {
+        return (
+            <h1>test</h1>
+        );
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
