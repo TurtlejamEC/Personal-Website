92c0261e36074c5811205b21c66b81f18e9a23b4
Elias Cho <33488019+TurtleBCA@users.noreply.github.com>
2020-03-23 18:06:11 -0400

    Basic foundations of Music page done

--- src/musicPageContent.js
+++ src/musicPageContent.js
@@ -1,5 +1,25 @@
 export const musicPageContent =
     <div className="musicPage">
-        <p>TODO</p>
+        <ul>
+            <li style={{padding: "3% 0%"}}>
+                <iframe className="featherEdges" width="560" height="315" src="https://www.youtube.com/embed/itWtakL2zjE" frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen style={{float:"right", borderRadius:"2%", verticalAlign:"middle", margin:"1em"}}/>
+                <h2>What kind of music do I make?</h2>
+                <p>I really make whatever I feel like. If I feel like making something along the lines of electro swing, I’ll make it. If I feel like making glitch hop, then I’ll go for it. If I feel like making an orchestral work, then I’ll work on it. It’s really whatever I feel inspired to make, classical or electronic. I guess that kind of makes me a jack of all trades :P I usually make my own songs, but when I feel inspired to, I remix game soundtracks.</p>
+            </li>
+            <li style={{padding: "3% 0%"}}>
+                <h2>What software do I use?</h2>
+                <p>Ableton Live for audio production and Musescore for notation.</p>
+            </li>
+            <li style={{padding: "1% 0%"}}>
+                <iframe className="featherEdges" width="560" height="315" scrolling="no" frameBorder="no" allow="autoplay"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1017496096&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+                        style={{float:"right", borderRadius:"2%", verticalAlign:"middle", margin:"1em"}}/>
+                <h2>Background</h2>
+                <p>I’ve always played piano, and picked up other instruments such as alto saxophone along the way. Piano will always be my favorite instrument though. :) Although I’ve performed and competed a lot, I’ve also composed for a very long time since I was quite little, and it has always been a hobby of mine (definitely more fun than practicing haha). When I began writing on Noteflight (I really don’t recommend it to anyone anymore), I picked up the name “Turtlejam”, a combination of my favorite animal and a jam in the musical context. Now, I use Ableton Live for audio production and Musescore for notation software.</p>
+                <p>I began my Youtube channel as a means of saving my compositions in a reliable space, but now I also use it to share my work with the world. I also have a <a target="_blank" rel="noopener noreferrer" href={"https://www.facebook.com/TurtlejamMusic/"}>Facebook page</a> (https://www.facebook.com/TurtlejamMusic/) and <a target="_blank" rel="noopener noreferrer" href={"https://soundcloud.com/elias-cho-3"}>SoundCloud account</a> (https://soundcloud.com/elias-cho-3) where I put my works.</p>
+            </li>
+        </ul>
     </div>
 ;
