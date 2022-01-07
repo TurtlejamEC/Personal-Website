ab7bdccb6225ed8c5a9492bb98ac6323641e91e6
Elias Cho <turtlejam.ec@gmail.com>
2022-01-07 11:52:02 -0500

    Header component

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 import './App.css';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
       </div>
     );
   }
--- src/containers/Router/Router.tsx
+++ src/containers/Router/Router.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from '../App/App';
 
 export default class Router extends React.Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<App />}>
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        )
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    )
+  }
 }
