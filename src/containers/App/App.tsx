612ff8edbd3fba4ad003ddeec22dae251057def5
Elias Cho <turtlejam.ec@gmail.com>
2022-01-07 11:47:07 -0500

    Convert App to class component

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
-
-export default App;
