0fba37b76d3841b61ec6b35209ad870a6b4b6fa2
Elias Cho <turtlejam.ec@gmail.com>
2022-01-10 16:16:47 -0500

    Center BasicInfo

--- src/components/BasicInfo/BasicInfo.tsx
+++ src/components/BasicInfo/BasicInfo.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './BasicInfo.css';
 
 export default class BasicInfo extends React.Component {
   render() {
     return (
-      <div className="BasicInfo center">
+      <div className="BasicInfo centerHorizontal">
         <h1>Elias Cho</h1>
         <p>Github: <a target="_blank" rel="noopener noreferrer" href="https://github.com/TurtleBCA">github.com/TurtleBCA</a></p>
         <p>Youtube: <a target="_blank" rel="noopener noreferrer" href="https://youtube.com/user/Aquaboost123/featured">youtube.com/user/Aquaboost123/featured</a></p>
