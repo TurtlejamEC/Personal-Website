1835cb8fa709a5dad8ca468471cde4b255cb74d3
Elias Cho <turtlejam.ec@gmail.com>
2022-01-11 13:31:48 -0500

    Set up ProjectsPage route

---
+++ src/components/ProjectsPage/ProjectsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+export default class ProjectsPage extends React.Component {
+  render() {
+    return (
+      <div id='ProjectsPage'>
+        <Outlet />
+      </div>
+    );
+  }
+}
--- src/containers/Router/Router.tsx
+++ src/containers/Router/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from '../App/App';
 import AboutPage from '../../components/AboutPage/AboutPage';
 import MusicPage from '../../components/MusicPage/MusicPage';
+import ProjectsPage from '../../components/ProjectsPage/ProjectsPage';
 
 export default class Router extends React.Component {
   render() {
@@ -11,6 +12,9 @@ export default class Router extends React.Component {
         <Routes>
           <Route path='/' element={<App />}>
             <Route path="music" element={<MusicPage />} />
+            <Route path="projects" element={<ProjectsPage />}>
+              
+            </Route>
             <Route index element={<AboutPage />} />
           </Route>
         </Routes>
