ace3ef486dfb0c5ed1ddcc25b155dbe7450ee5dd
Elias Cho <turtlejam.ec@gmail.com>
2022-01-11 13:12:26 -0500

    Set up MusicPage route

--- src/containers/Router/Router.tsx
+++ src/containers/Router/Router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from '../App/App';
 import AboutPage from '../../components/AboutPage/AboutPage';
+import MusicPage from '../../components/MusicPage/MusicPage';
 
 export default class Router extends React.Component {
   render() {
@@ -9,6 +10,7 @@ export default class Router extends React.Component {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<App />}>
+            <Route path="music" element={<MusicPage />} />
             <Route index element={<AboutPage />} />
           </Route>
         </Routes>
