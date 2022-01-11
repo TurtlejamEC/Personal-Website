ea0a32a54d4777e5c21389b23053fed437341093
Elias Cho <turtlejam.ec@gmail.com>
2022-01-10 19:43:22 -0500

    Update Github Link

--- src/components/BasicInfo/BasicInfo.tsx
+++ src/components/BasicInfo/BasicInfo.tsx
@@ -6,7 +6,7 @@ export default class BasicInfo extends React.Component {
     return (
       <div id="BasicInfo" className="centerHorizontal">
         <h1>Elias Cho</h1>
-        <p>Github: <a target="_blank" rel="noopener noreferrer" href="https://github.com/TurtleBCA">github.com/TurtleBCA</a></p>
+        <p>Github: <a target="_blank" rel="noopener noreferrer" href="https://github.com/TurtlejamEC">github.com/TurtlejamEC</a></p>
         <p>Youtube: <a target="_blank" rel="noopener noreferrer" href="https://youtube.com/user/Aquaboost123/featured">youtube.com/user/Aquaboost123/featured</a></p>
       </div>
     );
