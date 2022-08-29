924b850892ca8b87efc097d4859abbc83192e5fa
Elias Cho <turtlejam.ec@gmail.com>
2022-08-28 20:07:35 -0400

    Patch up missing pages

---
+++ src/components/BlogPage/BlogPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class BlogPage extends React.Component {
+  render() {
+    return (
+      <div id="BlogPage" className="page">
+        <p>Coming soon!</p>
+      </div>
+    );
+  }
+}
