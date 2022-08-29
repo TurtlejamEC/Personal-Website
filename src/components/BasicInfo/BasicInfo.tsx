5e6b0646edd70bee6558b32a74d02876717c5c14
Elias Cho <turtlejam.ec@gmail.com>
2022-08-28 20:46:59 -0400

    Add resume + basic info styling

--- src/components/BasicInfo/BasicInfo.tsx
+++ src/components/BasicInfo/BasicInfo.tsx
@@ -8,6 +8,7 @@ export default class BasicInfo extends React.Component {
         <h1>Elias Cho</h1>
         <p>Github: <a target="_blank" rel="noopener noreferrer" href="https://github.com/TurtlejamEC">github.com/TurtlejamEC</a></p>
         <p>Youtube: <a target="_blank" rel="noopener noreferrer" href="https://youtube.com/user/Aquaboost123/featured">youtube.com/user/Aquaboost123/featured</a></p>
+        <p>Resume: <a target="_blank" rel="noopener noreferrer" href="https://elias-cho-resume.s3.amazonaws.com/Resume.pdf">https://elias-cho-resume.s3.amazonaws.com/Resume.pdf</a></p>
       </div>
     );
   }
